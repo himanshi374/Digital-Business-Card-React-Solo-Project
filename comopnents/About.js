66adc7e401aa83f3a2deb48720346ab61c56acfd
himanshi374 <131516748+himanshi374@users.noreply.github.com>
2023-06-30 12:43:53 +0530

    Add files via Scrimba

---
+++ comopnents/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function About() {
+    return(
+        <div className="text-section">
+            <h4>About</h4>
+            <p>
+            I am a frontend web developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            
+            <h4>Interests</h4>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+        </div>
+    )
+}
